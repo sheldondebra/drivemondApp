8d775ae63aaa68c792816b62f875eed60dcc2b5e
sheldondebra <kofisheldon@gmail.com>
2024-05-14 18:04:53 +0000

    Pay update

--- Modules/Payment/Http/Controllers/PaymentController.php
+++ Modules/Payment/Http/Controllers/PaymentController.php
@@ -6,10 +6,10 @@ use Stripe\Charge;
 use Stripe\Stripe;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use App\Models\PaymentDetails;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\RedirectResponse;
-use Modules\Payment\Models\PaymentDetails;
 use Stripe\Exception\InvalidRequestException;
 
 class PaymentController extends Controller
@@ -25,91 +25,123 @@ class PaymentController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(Request $request)
     {
-        return view('payment::create');
+
+        if (auth()->check()) {
+            $user = Auth::user();
+
+            try {
+                $stripe = new \Stripe\StripeClient(config('stripe.secret'));
+
+                $response = $stripe->checkout->sessions->create([
+                    'line_items' => [
+                        [
+                            'price_data' => [
+                                'currency' => 'usd',
+                                'product_data' => [
+                                    'name' => "Drive Deposit",
+                                ],
+                                'unit_amount' => $request->price * 100,
+                            ],
+                            'quantity' => 1,
+                        ],
+                    ],
+                    'mode' => 'payment',
+                    'success_url' => route('success') . '?session_id={CHECKOUT_SESSION_ID}&uid=' . auth()->id(),
+                    'cancel_url' => route('cancel'),
+                ]);
+
+
+                if (isset($response->id) && $response->id != '') {
+                    return redirect($response->url);
+                } else {
+                    return redirect()->route('cancel');
+                }
+            } catch (InvalidRequestException $e) {
+                return response()->json(['error' => $e->getMessage()], 400);
+            }
+        } else {
+            return response()->json(['error' => 'User not authenticated '], 401);
+        }
     }
 
+    public function success(Request $request)
+    {
+        // dd($request->session_id);
+        if (isset($request->session_id)) {
+
+            try{
+                $stripe = new \Stripe\StripeClient(config('stripe.secret'));
+                $response = $stripe->checkout->sessions->retrieve($request->session_id);
+
+                $payment = PaymentDetails::create([
+                    'amount'=>($response->amount_total)/100,
+                    'transaction_id' => $response->id,
+                    'user_id' => $request->query('uid'),
+                    'email' => $response->customer_details->email,
+                    'status' => $response->status,
+                    'payment_method' => "Stripe",
+                ]);
+
+                return response()->json([
+                    'success' => true,
+                    'message' => 'Payment is successful.',
+                ], 200);
+            }
+            catch (\Exception $e) {
+                return response()->json(["error"=> $e->getMessage()], 400);
+            }
+
+        }
+        else {
+            return redirect()->route('cancel');
+        }
+    }
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function cancel(Request $request)
     {
-        if (Auth::check()) {
-            $user = Auth::user();
-            try {
-                Stripe::setApiKey(env('STRIPE_TEST_SK'));
-
-                $charge = Charge::create([
-                    'amount' => $request->amount,
-                    'currency' => 'usd',
-                    'source' => $request->stripeToken,
-                    'description' => 'Donation from ' . $request->name,
-                ]);
-
-                // Save payment details
-                $payment = PaymentDetails::create([
-                    'amount' => $request->amount,
-                    'payment_method' => 'Stripe', // Stripe is the only payment method
-                    'transaction_id' => $charge->id,
-                    'status' => 'completed', // Assuming payment is successful
-                    'email' => $request->email,
-                    'user_id' => $user->id,
-                ]);
-
-                // Save donation with payment details
-                // $donation = $payment->donation()->create([
-                //     'amount' => $request->amount,
-                //     'Driver_name' => $request->name,
-                //     'email' => $request->email,
-                //     'user_id' => $user->id,
-                //     // Other fields
-                // ]);
-
-                // Return success response or redirect
-                return response()->json(['message' => 'Payment successful']);
-            } catch (InvalidRequestException $e) {
-                // Handle Stripe validation errors
-                return response()->json(['error' => $e->getMessage()], 400);
-            } catch (\Exception $e) {
-                // Handle other exceptions
-                return response()->json(['error' => 'An error occurred while processing the donation'], 500);
-            }
-        } else {
-            // Handle case where user is not authenticated
-            return response()->json(['error' => 'User not authenticated'], 401);
-        }
+        return response()->json([
+            'success' => false,
+            'message' => 'Payment cancelled by the user.',
+        ], 400);
     }
 
     /**
      * Show the specified resource.
      */
-    public function show($id)
+    public function total_amount($id)
     {
-        return view('payment::show');
+
+        if(auth()->check()){
+            try{
+                $userPayments = PaymentDetails::where('user_id', $id)->sum('amount');
+
+                if ($userPayments === 0 || $userPayments == null) {
+                    return response()->json([
+                        'success' => true,
+                        'data' => 0,
+                        'message' => 'No payments found for the user.',
+                    ], 200);
+                }
+                return response()->json([
+                    'success' => true,
+                    'data' => $userPayments,
+                    'message' => 'Payments found for the user.',
+                ], 200);
+
+
+            }
+            catch(\Exception $e){
+                return response()->json(["error" => $e->getMessage()], 400);
+            }
+        }
+        else{
+            return response()->json(['error' => 'User not authenticated '], 401);
+        }
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit($id)
-    {
-        return view('payment::edit');
-    }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, $id): RedirectResponse
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
